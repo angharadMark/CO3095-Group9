14cb8c6e6398e14412e2be6b075ffa816b1a420d
CaitlinWatt-uni <cw536@student.le.ac.uk>
2025-11-12 20:59:07 +0000

    12/11/2025 - User can now add films to database

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from object.user import User
 from database.database import Database
 from database.database_loader import DatabaseLoader
+from database.database_writer import DatabaseWriter
+from object.film import Film
 
 def main():
     user = User("test")
     imports = DatabaseLoader()
     database = imports.load("films.json")
+    export = DatabaseWriter()
     while True:
+        print("\n")
         print("Welcome to the film reccomendation system!")
         print("1: Add a film to the database")
         print("2: Add a film to your watch list")
+        print("3: Exit")
+        print("\n")
 
         quest = int(input("Please select an option: "))
 
         if quest == 1:
-            pass
+            film = Film()
+            if film.input_film() == False:
+                del film
+            else:
+                database.add_films(film)
+
         elif quest == 2:
             film = input("Please input the film name you want to add: ")
             result = database.get_film(film)
@@ -26,7 +37,16 @@ def main():
                     result = database.get_film(film)
                 else:
                     break
+            result.display_film()
+            correct_check = input("Is this the correct film? Y/N : ").strip()
+            if correct_check.lower() == "n":
+                print("Film not added")
+                pass
             user.add_to_watchList(result)
+            print("Film added to watchlist!")
+        elif quest == 3:
+            break
+    export.upload(database,"films.json")
 
 
 
--- object/user.py
+++ object/user.py
@@ -2,10 +2,16 @@ class User:
     def __init__(self, username):
         self.username = username
         self.watchList=[]
+        self.films_added=0
 
     def add_to_watchList(self, film):
         self.watchList.append(film)
-        
+    
+    def get_films_added(self):
+        return self.films_added
+
+    def set_films_added(self, value):
+        self.films_added = value
 
 
 
