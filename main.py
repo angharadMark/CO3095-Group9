ce8bc1a5f957685228c2a49ee71721646617351e
Functionable <40835042+Functionable@users.noreply.github.com>
2025-11-20 16:54:25 +0000

    Add film rating feature

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
         print("Welcome to the film reccomendation system!")
         print("1: Add a film to the database")
         print("2: Add a film to your watch list")
+        print("3: Rate a film in your watchlist")
 
         quest = int(input("Please select an option: "))
 
@@ -27,12 +28,55 @@ def main():
                 else:
                     break
             user.add_to_watchList(result)
+        elif quest == 3:
+            rate_film_in_watchlist(user)
 
+def rate_film_in_watchlist(user):
+    user_watchlist = user.get_watch_list()
 
+    if len(user_watchlist) < 1:
+        print("Your watch list is empty! Please add some films to your watch list.")
+        return
 
+    while True:
+        print("Your watchlist ratings: ")
+        for idx, film in enumerate(user.get_watch_list()):
+            film_rating = user.get_rating(film.name)
+            rating_str = f"{film_rating}/10" if film_rating != None else "No Rating"
+            print(f"{idx+1}: {film.name} ({rating_str})")
 
+        print()
 
+        user_input = input("Please select a film to rate (write q to quit): ").strip().lower()
 
+        if user_input == 'q':
+            break
+
+        try:
+            list_idx = int(user_input) - 1
+            if list_idx < 0 or list_idx >= len(user_watchlist):
+                print("Unknown entry in watchlist")
+                continue
+        except ValueError:
+            print("Unknown entry in watchlist")
+            continue
+
+        selected_film = user_watchlist[list_idx]
+        print(f"Selected '{selected_film.name}'")
+
+        while True:
+            user_rating_input = input("Rate this film (0-10): ")
+
+            try:
+                user_rating = float(user_rating_input)
+                user.add_rating(selected_film.name, user_rating)
+                break
+            except ValueError:
+                print("Invalid value. Please try again.")
+            except IndexError:
+                print("Value not in range. Please try again.")
+
+        print("Film rating applied.")
 
 if __name__ == "__main__":
     main()
--- object/user.py
+++ object/user.py
@@ -3,9 +3,26 @@ class User:
         self.username = username
         self.watchList=[]
 
+        # A dictionary associating film names with their ratings (0-10)
+        self.ratings = {}
+
     def add_to_watchList(self, film):
         self.watchList.append(film)
-        
 
+    def add_rating(self, film_name, rating):
+        if rating < 0 or rating > 10:
+            raise IndexError("Rating value not in range 0-10")
+            return
+
+        self.ratings.update({film_name: rating})
+
+    def get_rating(self, film_name):
+        if not film_name in self.ratings:
+            return None
+
+        return self.ratings[film_name]
+        
+    def get_watch_list(self):
+        return self.watchList
 
 
